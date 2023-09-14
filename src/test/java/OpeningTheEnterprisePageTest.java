145f2cbb823f4bbeca7b1497262d69986e6e214b
a.chernyakova <a.chernyakova@lipt-soft.ru>
2023-09-14 11:25:45 +0300

    add the whole project

---
+++ src/test/java/OpeningTheEnterprisePageTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OpeningTheEnterprisePageTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void openPageAndFoundHeaderTest() {
+        // открыть главную страницу
+        open(Configuration.baseUrl);
+        // найти в меню Solutions -> Enterprise
+        $(byTagAndText("button", "Solutions")).hover();
+        // перейти на страницу Enterprise
+        $(byText("Enterprise")).click();
+        // проверить, что открылась нужная страница
+        $(".application-main").shouldHave(text("Build like the best"));
+    }
+}
